63d152d4a426cb32a5b1eb33ac5116ad8ae9a7ea
LENOVO <LENOVO@LAPTOP-4LNFGR6C>
2024-08-03 17:29:15 +0530

    First commit

---
+++ DomoClone/src/Base/BasePage.java
@@ -0,0 +1,25 @@
+package Base;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage extends BaseTest {
+	
+	WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+	
+	public WebElement waitForElementToBeVisible(WebElement selector) {
+		return wait.until(ExpectedConditions.visibilityOf(selector));
+	} 
+	public void sleep(int sec) {
+		System.out.println("Sleeping");
+		try {
+			TimeUnit.SECONDS.sleep(sec);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
